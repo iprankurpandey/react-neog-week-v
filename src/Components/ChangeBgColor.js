27846b0df9cbfec49b8c5439f17fa428fb9017be
iprankurpandey <pprankur@gmail.com>
2022-03-07 23:39:07 +0530

    1 file modified

--- src/Components/ChangeBgColor.js
+++ src/Components/ChangeBgColor.js
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import React from 'react';
 function ChangeBgColor() {
-  const [color, setColor] = useState('red');
-  const Newcolor = setColor('red') ? 'Green' : 'red';
+  const [color, setColor] = useState('');
+  const changeColor = () =>
+    setColor('red') ? setColor('green') : setColor('red');
 
   return (
     <div style={{ backgroundColor: color }}>
       <h1>This is the paragraph we are changing.</h1>
-      <button onClick={ChangeBgColor}> change color</button>
+      <button onClick={changeColor}> change color</button>
     </div>
   );
 }
