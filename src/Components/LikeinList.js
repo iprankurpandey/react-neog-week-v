5f57681165a3b1bee50c7905fcb65ae3c029ea55
iprankurpandey <pprankur@gmail.com>
2022-03-08 23:05:44 +0530

    done with list item

--- src/Components/LikeinList.js
+++ src/Components/LikeinList.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { useState } from 'react';
 
 function LikeinList() {
-  return <div></div>;
+  const [list, setList] = useState('click to render list');
+  function ListLikes() {
+    const listItem = ['Apple', 'Mango', 'Banana'];
+    const mapItem = listItem.map((e) => <li> {e + '❤️️'} </li>);
+    setList(mapItem);
+  }
+  return (
+    <div>
+      <ul> {list}</ul>
+      <button onClick={ListLikes}>render</button>
+    </div>
+  );
 }
 export default LikeinList;
