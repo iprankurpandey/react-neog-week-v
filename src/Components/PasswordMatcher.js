22bac19e809b045d98bb87fba619860ac1f7397b
iprankurpandey <pprankur@gmail.com>
2022-03-08 09:30:46 +0530

    done with password app

--- src/Components/PasswordMatcher.js
+++ src/Components/PasswordMatcher.js
@@ -4,13 +4,9 @@ import { useState } from 'react';
 function PasswordMacther() {
   const [value, setValue] = useState('enter password ');
   function matchPassword(event) {
-    let input1 = document.getElementsByClassName('input1');
-    input1 = event.target.value.length;
-    console.log(input1);
-    let input2 = document.getElementsByClassName('input2').value;
-    input2 = event.target.value.length;
-
-    if (input1 && input2 > 8) {
+    let input1 = document.querySelector('.input1');
+    let input2 = document.querySelector('.input2');
+    if (input1.value.length === input2.value.length) {
       setValue('matched');
     } else {
       setValue('not macthed');
@@ -20,7 +16,7 @@ function PasswordMacther() {
     <div>
       <h2>Password : {value} </h2>
       <input onChange={matchPassword} className="input1" />
-      <input onChange={matchPassword} className="input1" />
+      <input onChange={matchPassword} className="input2" />
     </div>
   );
 }
